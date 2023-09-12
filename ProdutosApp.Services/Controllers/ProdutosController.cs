8deb24d9238baf35e4d36edb0c8c2b64f9f82a60
smendescoti <sergio.coti@gmail.com>
2023-09-11 21:37:42 -0300

    first commit

---
+++ ProdutosApp.Services/Controllers/ProdutosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApp.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        /// <summary>
+        /// Método para cadastrar produtos na API
+        /// </summary>
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método para atualizar produtos na API
+        /// </summary>
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método para excluir produtos na API
+        /// </summary>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método para consultar produtos na API
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
